d79069bb5fd66959d5b73e1ad0d1dd6132716d8b
Christian Casey <christiancasey86@gmail.com>
2022-10-08 21:11:28 +0200

    Tested pulling glyph data from Grist via Napkin

--- hieroglyphsirl/src/App.js
+++ hieroglyphsirl/src/App.js
@@ -1,13 +1,30 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [signList, setSignList] = React.useState([]);
+
+  const url = "https://hieroglyphsirl.npkn.net/signlist/";
+  React.useEffect(() => {
+    fetch(url)
+    .then(resp => resp.json())
+    .then(data => {
+      console.log(data);
+      setSignList(data.records);
+    })
+    .catch(err => console.log(err));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>HieroglyphsIRL</h1>
         <p>Coming soon...</p>
         <img src={logo} className="App-logo" alt="logo" />
+        <div className='signListContainer'>
+          {signList.map(sign => (<p key={sign.id}>{sign.fields.unicode}</p>))}
+        </div>
       </header>
     </div>
   );
